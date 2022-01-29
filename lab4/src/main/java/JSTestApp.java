ba8e5481a09aaed01fa6020fb36bb25707ae3969
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-01-30 01:07:54 +0300

    Scripted auto-commit on change (2022-01-30 01:07:54) by gitwatch.sh

--- lab4/src/main/java/JSTestApp.java
+++ lab4/src/main/java/JSTestApp.java
@@ -1,5 +1,13 @@
 package main.java;
 
+import java.io.IOException;
+
 public class JSTestApp extends AllDirectives {
-    public
+    public static final String ACTOR_SYSTEM_NAME = "js_test_app";
+
+    public static void main(String[] args) throws IOException {
+        ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
+
+
+    }
 }
