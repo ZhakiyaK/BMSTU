6bfddd2bf537e31f95887c3cacc5c125dd66096a
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-01-30 02:01:36 +0300

    Scripted auto-commit on change (2022-01-30 02:01:36) by gitwatch.sh

--- lab4/src/main/java/ActorRouter.java
+++ lab4/src/main/java/ActorRouter.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import static java.security.AccessController.getContext;
+
 public class ActorRouter extends AbstractActor {
     private static final int TESTERS_AMOUNT = 5;
 
@@ -7,6 +9,7 @@ public class ActorRouter extends AbstractActor {
     private final Router router;
 
     {
-        keeper = getContext().actorOf(Props.create(ActorKeeper.class))
+        keeper = getContext().actorOf(Props.create(ActorKeeper.class));
+
     }
 }
