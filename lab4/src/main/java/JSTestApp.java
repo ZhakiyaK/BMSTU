0cb9710a335e354fab9a02d3f8986a05c625eb3c
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-01-30 01:14:04 +0300

    Scripted auto-commit on change (2022-01-30 01:14:04) by gitwatch.sh

--- lab4/src/main/java/JSTestApp.java
+++ lab4/src/main/java/JSTestApp.java
@@ -7,7 +7,10 @@ public class JSTestApp extends AllDirectives {
 
     public static void main(String[] args) throws IOException {
         ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
-        ActorRef actorRouter = actorSystem.actorOf(Props.create(ActorRouter,class));
+        ActorRef actorRouter = actorSystem.actorOf(Props.create(ActorRouter.class));
+
+        final Http http = Http.get(actorSystem);
+        final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
 
 
     }
