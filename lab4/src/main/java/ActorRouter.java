b3945403363739714153fe976eb24f5b7db9cdf1
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-01-30 02:04:22 +0300

    Scripted auto-commit on change (2022-01-30 02:04:22) by gitwatch.sh

--- lab4/src/main/java/ActorRouter.java
+++ lab4/src/main/java/ActorRouter.java
@@ -1,6 +1,8 @@
 package main.java;
 
-import static java.security.AccessController.getContext;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActorRouter extends AbstractActor {
     private static final int TESTERS_AMOUNT = 5;
@@ -10,6 +12,10 @@ public class ActorRouter extends AbstractActor {
 
     {
         keeper = getContext().actorOf(Props.create(ActorKeeper.class));
+        List<Routee> routees = new ArrayList<>();
+        for (int i=0; i < TESTERS_AMOUNT; i++) {
+            ActorRef r = getContext().actorOf(PropertyChangeSupport)
+        }
 
     }
 }
