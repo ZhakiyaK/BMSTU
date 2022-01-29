463ccbc5f21b9e44e09fa6d35a652aa5e8ff4e8d
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-01-30 01:47:14 +0300

    Scripted auto-commit on change (2022-01-30 01:47:14) by gitwatch.sh

--- lab4/src/main/java/JSTestApp.java
+++ lab4/src/main/java/JSTestApp.java
@@ -1,6 +1,12 @@
 package main.java;
 
 import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Future;
+import java.util.regex.Pattern;
+
 import akka.hhtp.javadsl.server.AllDirectives;
 public class JSTestApp extends AllDirectives {
     public static final String ACTOR_SYSTEM_NAME = "js_test_app";
@@ -11,7 +17,50 @@ public class JSTestApp extends AllDirectives {
 
         final Http http = Http.get(actorSystem);
         final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
+        JSTestApp instance = new JSTestApp();
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute(actorRouter).flow(actorSystem, materializer);
+        final CompletionStage<ServerBinding> binding;
+        binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(host:"localhost", port: 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind);
+                .thenAccept(unbound -> actorSystem.terminate());
+    }
 
+    private Route createRoute(ActorRef actorRouter) {
+        return route(
+                path(segment:"test", () ->
+                      route(
+                              post(() ->
+                                        entity(Jacson.unmarshaller(MessageTestPackage.class), message -> {
+           actorRouter.tell(message.ActorRef.noSender());
+           return complete(body:"Test started!");
+                                        }))
 
+        )),
+        path(segment:"result". () ->
+                route(
+                        get(
+                                () -> parameter(name: "packageId". (id) -> {
+                                    Future<Object> result = Pattern.ask(
+                                            actorRouter,
+                                            new MessageGetTestPackageResult(id),
+                                            timeoutMillis: 5000
+                                    );
+                                    return completeOKWithFuture(result, Jackson,marshaller());
+                                 })
+                        )
+                )
+        )
+            );
+    }
+
+    static class MessageGetTestPackageResult {
+        private final String packageID;
     }
 }
