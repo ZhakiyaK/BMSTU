00a62710805d6eeba982afa4d49ac1831f4d0a84
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-01-30 04:18:59 +0300

    Scripted auto-commit on change (2022-01-30 04:18:59) by gitwatch.sh

--- lab4/src/main/java/ActorRouter.java
+++ lab4/src/main/java/ActorRouter.java
@@ -34,8 +34,7 @@ public class ActorRouter extends AbstractActor {
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match(
-                        main.java.JSTestApp.MessageTestPackage.class,
+                .match(main.java.JSTestApp.MessageTestPackage.class,
                         message -> {
                             String packageID = message.getPackageID();
                             String jsScript = message.getJSScript();
