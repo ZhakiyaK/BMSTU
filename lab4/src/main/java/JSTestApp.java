6fef63176c6bfa39b18576cb7b6a19888c56c680
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-01-30 03:42:07 +0300

    Scripted auto-commit on change (2022-01-30 03:42:07) by gitwatch.sh

--- lab4/src/main/java/JSTestApp.java
+++ lab4/src/main/java/JSTestApp.java
@@ -7,15 +7,16 @@ import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.IOException;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Future;
@@ -90,6 +91,7 @@ public class JSTestApp extends AllDirectives {
         private final String funcName;
         private final List<TestBody> tests;
 
+        @JsonCreator
         public MessageTestPackage(
                 @JsonProperty("packageID") String packageID,
                 @JsonProperty("jsScript") String jsScript,
