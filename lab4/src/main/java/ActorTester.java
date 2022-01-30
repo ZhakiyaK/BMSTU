c361f6b37189cd537b93619070a3a238a065a4cb
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-01-30 03:12:30 +0300

    Scripted auto-commit on change (2022-01-30 03:12:30) by gitwatch.sh

--- lab4/src/main/java/ActorTester.java
+++ lab4/src/main/java/ActorTester.java
@@ -1,4 +1,18 @@
 package main.java;
 
 public class ActorTester extends AbstractActor {
+    private static final String SCRIPT_ENGINE_NAME = "nashorn";
+    private static final String TEST_PASSED_STATTUS = "PASSED";
+    private static final String TEST_FAILED_STATUS = "FAILED";
+    private static final String TEST_CRASHED_STATUS = "CRASHED";
+    private static final String EMPTY_STRING = "";
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(ActorRouter.MessageTest.class, m -> sender().tell(runTest(m), self() ) )
+                .build();
+    }
+
+    private String execJS(String jsScript)
 }
--- lab4/src/main/java/TestBody.java
+++ lab4/src/main/java/TestBody.java
@@ -1,4 +1,29 @@
 package main.java;
 
 public class TestBody {
+    private final String testName;
+    private final Object[] params;
+    private final String expectedResult;
+
+    @JsonCreator
+    public TestBody(
+            @JsonProperty("testName") String testName,
+            @JsonProperty("params") Object[] params,
+            @JsonProperty("expectedResult") String expectedResult) {
+        this.testName = testName;
+        this.params = params;
+        this.expectedResult = expectedResult;
+    }
+
+    protected String getTestName() {
+        return testName
+    }
+
+    protected  Object[] getParams() {
+        return params;
+    }
+
+    protected String getExpectedResult() {
+        return expectedResult;
+    }
 }
