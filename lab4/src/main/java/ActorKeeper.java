dd192625aaeefc3d44b1ae0030144b0152645210
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-01-30 03:03:20 +0300

    Scripted auto-commit on change (2022-01-30 03:03:20) by gitwatch.sh

--- lab4/src/main/java/ActorKeeper.java
+++ lab4/src/main/java/ActorKeeper.java
@@ -1,8 +1,64 @@
 package main.java;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ActorKeeper extends AbstractActor {
-    private final Map<String, List<TestResult>>
+    private final Map<String, List<TestResult>> results = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(ActorTester.MessageStoreTestResult.class,
+                        this.storeResult
+                )
+                .match(
+                        JSTestApp.MessageGetTestPackageResult.class,
+                        req -> sender().tell(
+                                new MessageReturnResults(
+                                        req.getPackageID(),
+                                        results.get(req.getPackageID())
+                                ),
+                                self()
+                        )
+                )
+                .build();
+    }
+
+    private void storeResult(ActorTester.MessageStoreTestResult m) {
+        String packageID = m.getPackageID();
+        if (results.containsKey(packageID)) {
+            results.get(packageID).add(m.getTestResult());
+        }
+        else {
+            results.put(
+                    m.getPackageId(),
+                    new ArrayList<>(
+                            Collections.singleton(m.getTestResult())
+                    )
+            );
+        }
+        System.out.println("Received message: " + m);
+    }
+
+    static class MessageReturnResults {
+        private final String packageID;
+        private final List<TestResult> results;
+
+        @JsonCreator
+        public MessageReturnResults(
+                @JsonProperty("packageId") String packageID;
+                @JsonProperty("results") List<TestResult> results) {
+            this.packageID = packageID;
+            this.results = results;
+        }
+
+        public String getPackageID() {
+            return packageID;
+        }
+
+        public List<TestResult> getResults() {
+            return results;
+        }
+
+    }
 }
--- lab4/src/main/java/TestResult.java
+++ lab4/src/main/java/TestResult.java
@@ -1,4 +1,44 @@
 package main.java;
 
 public class TestResult {
+    private final String status;
+    private final String testName;
+    private final String expectedResult;
+    private final String receivedREsult;
+
+    @JsonCreator
+    public TestResult(@JsonProperty("status") String status,
+                      @JsonProperty("testName") String testName,
+                      @JsonProperty("expectedResult") String expectedResult,
+                      @JsonProperty("receivedResult") String receivedREsult) {
+        this.status = status;
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.receivedREsult = receivedREsult;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public String getReceivedREsult() {
+        return receivedREsult;
+    }
+
+ @Override
+ public String toString() {
+        return "Status: " + status + "\n"
+                + "Test name: " + testName + "\n"
+                + "Expected: " + expectedResult + "\n"
+                + "Received: " + receivedREsult + "\n";
+ }
+
 }
