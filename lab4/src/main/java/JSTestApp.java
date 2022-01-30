382c793c6c7745eb91b068905c6093fa09bfc3a4
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-01-30 04:02:33 +0300

    Scripted auto-commit on change (2022-01-30 04:02:33) by gitwatch.sh

--- lab4/src/main/java/JSTestApp.java
+++ lab4/src/main/java/JSTestApp.java
@@ -17,7 +17,6 @@ import akka.stream.javadsl.Flow;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.TestBody;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
