19d9788a726ff61702d9f77dd92eb7af790b50ca
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-02-02 02:35:38 +0300

    Scripted auto-commit on change (2022-02-02 02:35:38) by gitwatch.sh

--- lab5/src/main/java/AverageHttpResponseTimeApp.java
+++ lab5/src/main/java/AverageHttpResponseTimeApp.java
@@ -1,9 +1,15 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+import java.io.IOException;
+
 public class AverageHttpResponseTimeApp {
     private static final String QUERY_PARAMETER_URL = "testUrl";
     private static final String QUERY_PARAMETER_COUNT = "count";
 
     private static final int TIMEOUT_MILLISEC = 5000;
-    private static final int MAP_PARALLELISM_FOR EACH_GET_REQUEST = 1;
+    private static final int MAP_PARALLELISM_FOR_EACH_GET_REQUEST = 1;
     private static final int PPORT = 8080;
 
     public static void main(String[] args) throws IOException {
