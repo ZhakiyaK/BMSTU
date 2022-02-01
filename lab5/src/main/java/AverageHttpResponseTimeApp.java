695c3020e46c08ee340fc76fdf236e2ce4ab272c
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-02-02 02:31:57 +0300

    Scripted auto-commit on change (2022-02-02 02:31:57) by gitwatch.sh

--- lab5/src/main/java/AverageHttpResponseTimeApp.java
+++ lab5/src/main/java/AverageHttpResponseTimeApp.java
@@ -1,3 +1,16 @@
 public class AverageHttpResponseTimeApp {
-    private 
+    private static final String QUERY_PARAMETER_URL = "testUrl";
+    private static final String QUERY_PARAMETER_COUNT = "count";
+
+    private static final int TIMEOUT_MILLISEC = 5000;
+    private static final int MAP_PARALLELISM_FOR EACH_GET_REQUEST = 1;
+    private static final int PPORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        ActorRef actor = system.actorOf(Props.create(ActorCache.class));
+
+        
+    }
 }
