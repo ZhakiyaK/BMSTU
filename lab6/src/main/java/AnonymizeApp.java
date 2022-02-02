d51df89446be14e3e1089a6abbe1c033791f44f5
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-02-02 08:08:02 +0300

    Scripted auto-commit on change (2022-02-02 08:08:02) by gitwatch.sh

--- lab6/src/main/java/AnonymizeApp.java
+++ lab6/src/main/java/AnonymizeApp.java
@@ -1,3 +1,13 @@
 public class AnonymizeApp {
-    public static final String ANSI_GREEN
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESR = "\u001B[0m";
+
+    private static final int MIN_AMOUNT_OF_ARGS = 2;
+    private static final int ZOOKEEPER_SESSION_TIMEOUT = 3000;
+    private static final int INDEX_OF_ZOOKEEPER_ADDRESS_IN_ARGS = 0;
+    private static final int NO_SERVERS_RUUNING = 0;
+    private static final String HOST = "localhost";
+    private static final String NO_SERVERS_RUNNING_ERROR = "NO SERVERS ARE RUNNING";
+
+    public static void main(String[] args) throws IOException 
 }
