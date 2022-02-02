a975de91ead3b7bac767c1cb17d19400a21550f6
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-02-02 03:27:58 +0300

    Scripted auto-commit on change (2022-02-02 03:27:58) by gitwatch.sh

--- lab5/src/main/java/AverageHttpResponseTimeApp.java
+++ lab5/src/main/java/AverageHttpResponseTimeApp.java
@@ -112,4 +112,22 @@ public class AverageHttpResponseTimeApp {
             return url;
         }
     }
+
+    static class MessageCacheResult {
+        private final String url;
+        private final long responseTime;
+
+        public MessageCacheResult(String, url, long responseTime) {
+            this.url = url;
+            this.responseTime = responseTime;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public long getResponseTime() {
+            return responseTime;
+        }
+    }
 }
