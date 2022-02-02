6d691e3d6aac81bd0a84f2099cbc4eed6db00323
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-02-02 03:25:13 +0300

    Scripted auto-commit on change (2022-02-02 03:25:13) by gitwatch.sh

---
+++ lab5/src/main/java/ActorCache.java
@@ -0,0 +1,17 @@
+import akka.actor.AbstractActor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class ActorCache extends AbstractActor {
+    private final Map<String, Long> results = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(AverageHttpResponseTimeApp.MessageGetResult.class, message -> sender().tell(Optional.ofNullable(results.get(message.getUrl()))), self())
+                .match(AverageHttpResponseTimeApp.MessageGetResult.class, message -> results.put(message.getUrl(), message.getResponseTime()))
+                .build();
+    }
+}
