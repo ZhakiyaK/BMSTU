fa80d1bc80716a0ac09d040e0b0fda7cfa71bc6b
ZhakiyaK <akzhol.zhakiya@gmail.com>
2022-02-03 20:40:17 +0300

    Scripted auto-commit on change (2022-02-03 20:40:17) by gitwatch.sh

--- lab6/src/main/java/AnonymizeApp.java
+++ lab6/src/main/java/AnonymizeApp.java
@@ -1,3 +1,13 @@
+
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import com.sun.tools.javac.util.BasicDiagnosticFormatter;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 public class AnonymizeApp {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RESR = "\u001B[0m";
@@ -9,5 +19,14 @@ public class AnonymizeApp {
     private static final String HOST = "localhost";
     private static final String NO_SERVERS_RUNNING_ERROR = "NO SERVERS ARE RUNNING";
 
-    public static void main(String[] args) throws IOException 
+    public static void main(String[] args) throws IOException {
+        if (args.length < MIN_AMOUNT_OF_ARGS) {
+            System.err.println("Usage: AnonymizeApp localhost: 2181 8000 8001");
+            System.exit(-1);
+        }
+        BasicDiagnosticFormatter.BasicConfiguration();
+        printInGreen("start!\n" + Arrays.toString(args));
+        ActorSystem system = ActorSystem.create("lab6");
+        ActorRef actorConfig = system.actorOf(Props.create(ActorConfig.class))
+    }
 }
